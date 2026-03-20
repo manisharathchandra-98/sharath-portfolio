a1fb223d005b367a08ce6a1c1b5af6798aa19cce
manisharathchandra-98 <sattumanisharathchandra@gmail.com>
2026-03-19 21:45:39 -0400

    fix splitText TypeScript errors

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -1,6 +1,6 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
+import { ScrollSmoother } from "gsap/ScrollSmoother";
 import { SplitText } from "gsap-trial/SplitText";
 
 interface ParaElement extends HTMLElement {
@@ -26,13 +26,14 @@ export default function setSplitText() {
       para.split?.revert();
     }
 
-    para.split = new SplitText(para, {
+    const split = new SplitText(para, {
       type: "lines,words",
       linesClass: "split-line",
     });
+    para.split = split;
 
     para.anim = gsap.fromTo(
-      para.split.words,
+      split.words,
       { autoAlpha: 0, y: 80 },
       {
         autoAlpha: 1,
@@ -53,12 +54,13 @@ export default function setSplitText() {
       title.anim.progress(1).kill();
       title.split?.revert();
     }
-    title.split = new SplitText(title, {
+    const titleSplit = new SplitText(title, {
       type: "chars,lines",
       linesClass: "split-line",
     });
+    title.split = titleSplit;
     title.anim = gsap.fromTo(
-      title.split.chars,
+      titleSplit.chars,
       { autoAlpha: 0, y: 80, rotate: 10 },
       {
         autoAlpha: 1,
